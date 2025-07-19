f44f6293e213b04eedd33891c00493d67b87f914
mario <somsos112@gmail.com>
2025-07-18 18:36:30 -0600

    changed url to host.docker.internal port 8080

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   img404: '/img/404.png',
   backend: {
     mock: false,
-    path: 'http://host.docker.internal:8201',
+    path: 'http://host.docker.internal:8080',
   }
 
 };
